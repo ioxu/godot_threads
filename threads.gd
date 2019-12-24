737ce0c1758e62c4b510e3c7e95e21068fd53fb5
benp <ben@ioxu.com>
2019-12-25 01:34:24 +1030

    moved over to using RichTextLabel as log output display

--- threads.gd
+++ threads.gd
@@ -32,8 +32,16 @@ func _on_do_work_button_button_up():
 func _on_thread_log(data):
 	print(str(data))
 	log_output.text += str(data) + "\n"
-	log_output_rich.set_bbcode( log_output_rich.get_bbcode() + "[code]" + str(data) + "[/code]\n" )
 
+	# this bbcode formatting is cruising for a bruising --
+	var data_split = str(data).split(" ")
+	if data_split[0] == "start":
+		#var c = Color(1, 0.294118, 0.623529)
+		data =  "[color=#5aff48]"+data_split[0]+"[/color] "+ array_join(data_split, 1, " ")
+	elif data_split[0] == "end":
+		data =  "[color=#ff4b9f]"+data_split[0]+"[/color] "+ array_join(data_split, 1, " ")
+	log_output_rich.set_bbcode( log_output_rich.get_bbcode() + "[code]" + str(data) + "[/code]\n" )
+	# ----------------------------------------------------
 	
 	self._on_log_output_cursor_changed()
 
@@ -56,6 +64,14 @@ func _on_max_processors_spinbox_value_changed(value):
 	_on_thread_log("max processors changed to %s"%[value])
 
 # UTIL -----------------
+func array_join(arr,i_start ,sep = ' '):
+	var string = ''
+	for index in range(i_start, arr.size()):
+		string += str(arr[index])
+		if index < arr.size() - 1:
+			string += sep
+	return string
+
 func get_pretty_time():
 	# https://godotengine.org/qa/19077/how-to-get-the-date-as-per-rfc-1123-date-format-in-game
 	# u/jospic
